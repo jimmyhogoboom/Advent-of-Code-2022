b3c99f6217d53c2625d6b8dc2426c828f25b5e79
Jimmy Hogoboom <jimmy@differential.com>
2023-02-17 16:27:24 -0600

    organize

--- Main.hs
+++ Main.hs
@@ -13,6 +13,8 @@ main = do
       part2 elves
     _ -> print "whoops"
 
+-- Parts --
+
 part1 :: [Elf] -> IO ()
 part1 elves = do
   putStrLn "\nDay 1.1: Most calories: "
@@ -23,9 +25,13 @@ part2 elves = do
   putStrLn "\nDay 1.2: Top 3 snack-holders total: "
   print $ topSnackHoldersTotal elves 3
 
+-- Types --
+
 type Snack = Int
 
 newtype Elf = Elf {snacks :: [Snack]} deriving (Show)
+
+-- Logic --
 
 snackCalories :: Elf -> Int
 snackCalories = sum . snacks
