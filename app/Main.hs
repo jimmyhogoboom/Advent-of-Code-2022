118387241c32ac36f626a56534bc54cf66eee44c
Jimmy Hogoboom <jimmy@differential.com>
2023-02-18 00:43:38 -0600

    detect strings of text as invalid input

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,17 @@
 import qualified Day1.Snacks as Day1
 import qualified Day2.RPS as Day2
 import System.Environment (getArgs)
+import Text.Read (readMaybe)
 
 main :: IO ()
 main = do
-  args <- map read <$> getArgs
+  args <- map (readMaybe :: String -> Maybe Int) <$> getArgs
   case args of
-    [1, 1] ->
+    [Just 1, Just 1] ->
       Day1.part1
-    [1, 2] ->
+    [Just 1, Just 2] ->
       Day1.part2
-    [_, _] -> putStrLn "That part isn't done yet"
-    _ -> putStrLn "Invalid input. Use: `-- 1 1` to run Day 1 Part 1"
+    [Just x, Just y] ->
+      putStrLn "That part isn't done yet"
+    _ ->
+      putStrLn "Invalid input. Use: `-- 1 1` to run Day 1 Part 1"
